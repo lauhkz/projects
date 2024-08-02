abacd4e39a841ff862c10b52643eeb2f9ed9ae87
lauhkz <lautaro_RL@outlook.com>
2024-08-02 20:41:58 -0300

    added a readme with for the grocery_shopping exercise and did a bit of it

---
+++ boot_dev/grocery_shopping.md
@@ -0,0 +1,9 @@
+# The function should return 3 values in order:
+
+   - [x] ~~receipt: A dictionary containing all the items Emma purchased, even stuff not on her list. The keys are the items names~~
+
+   - [ ] unpurchased_items: a list of all the item names in grocery_list that weren't found in items_purchased in order.
+   and values are their respective prices from the item_prices dictionary.
+
+   - [x] ~~total: the total cost of all the items that were purchased.~~
+
---
+++ boot_dev/grocery_shopping.py
@@ -0,0 +1,30 @@
+items_purchased = ['milk', 'eggs', 'cheese', 'apples', 'bananas', 'lettuce', 'cereal']
+grocery_list = ['milk', 'oatmeal', 'eggs', 'cheese', 'apples', 'bananas', 'carrots', 'lettuce', 'potatoes', 'cereal', 'chicken']
+
+def calculate_total (items_purchased, grocery_list):
+    item_prices = {
+        "milk": 2.50,
+        "eggs": 3.25,
+        "bread": 1.21,
+        "cheese": 3.50,
+        "apples": 7.44,
+        "bananas": 3.88,
+        "carrots": 3.89,
+        "lettuce": 1.12,
+        "potatoes": 32.21,
+        "cereal": 5.99,
+    }
+    purchased_items = {}
+    total = 0
+    unpurchased_items = {}
+
+    for item in items_purchased:
+        if item in item_prices:
+            purchased_items[item] = item_prices[item]
+            total += item_prices[item]
+
+    print(purchased_items)
+    print(f"this is the total {total}")
+
+
+calculate_total(items_purchased, grocery_list)
